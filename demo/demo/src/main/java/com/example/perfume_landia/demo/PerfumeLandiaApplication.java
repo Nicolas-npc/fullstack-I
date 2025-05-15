a54f4108e387acb87872a5f4203b90931597acb1
Nicolas-npc <nicola.sanchezm@duocuc.cl>
2025-05-14 22:43:33 -0400

    Add files via upload

---
+++ demo/demo/src/main/java/com/example/perfume_landia/demo/PerfumeLandiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.perfume_landia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeLandiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumeLandiaApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/com/example/perfume_landia/demo/PerfumeLandiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perfume_landia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeLandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
